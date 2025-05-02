911cbd514d1f57e85234b90e859fec565b323829
Anushka Dubey <136051483+anushkaa-dubey@users.noreply.github.com>
2025-05-02 19:07:16 +0530

    day 1

--- DAY1/program1.java
+++ DAY1/program1.java
@@ -1,3 +1,23 @@
 // Today's Challenge:
 // Topic: Linked List
 // Link: https://leetcode.com/problems/reverse-linked-list/
+class Solution {
+    public ListNode reverseList(ListNode head) {
+     ListNode next;
+     ListNode current = head;
+     ListNode previous = null;
+     if(head == null)
+     {
+        return null;
+     }   
+     while( current != null)
+     {
+        next=current.next;
+        current.next= previous;
+        previous = current;
+        current = next;
+     }
+     return previous;
+
+    }
+}
--- DAY1/program2.java
+++ DAY1/program2.java
@@ -1,2 +1,11 @@
 // Topic: Math based
 // Link: https://leetcode.com/problems/missing-number/
+class Solution {
+    public int missingNumber(int[] nums) {
+    int len = nums.length;
+    int sum = (0+len)*(len+1)/2;
+    for(int i=0; i<len; i++)
+        sum-=nums[i];
+    return sum;
+}
+}
