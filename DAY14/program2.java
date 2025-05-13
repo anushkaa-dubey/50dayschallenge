e4d3fab4a6a8d6f48df56e8e7b813a6a682cae2c
anushkaa-dubey <anushka25dubey@gmail.com>
2025-05-13 23:23:27 +0530

    day14

---
+++ DAY14/program2.java
@@ -0,0 +1,14 @@
+// Maths based
+// https://leetcode.com/problems/reverse-integer/description/
+class Solution {
+    public int reverse(int x) {
+        long ans= 0;
+
+        while (x != 0){
+            ans = ans * 10 + x % 10;
+            x /= 10;
+
+        }
+        return (ans< Integer.MIN_VALUE|| ans> Integer.MAX_VALUE) ? 0 : (int) ans;
+    }
+}
