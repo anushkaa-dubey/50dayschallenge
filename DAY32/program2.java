3a649f8db06ff1bb14996e15f4f9144922764240
anushkaa-dubey <anushka25dubey@gmail.com>
2025-05-31 17:58:29 +0530

    day32

---
+++ DAY32/program2.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/rotate-image/description/
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        //transpose
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        //reverse rows
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix[0].length-1-j];
+                matrix[i][matrix[0].length-1-j]=temp;
+            }
+        }
+    }
+}
