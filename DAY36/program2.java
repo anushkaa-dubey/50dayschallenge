6baf468a8bcbeaf098e59ba1f0d2a1f186ea514e
anushkaa-dubey <anushka25dubey@gmail.com>
2025-06-04 23:34:23 +0530

    day36

---
+++ DAY36/program2.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+class Solution {
+    int sum=0;
+    public int sumNumbers(TreeNode root) {
+        helper(root,"");
+        return sum;
+    }
+    public void helper(TreeNode root,String str){
+        if(root==null){
+            return;
+        }
+        str+=root.val;
+        if(root.left==null && root.right==null){
+            sum+=Integer.parseInt(str);
+            return;
+        }
+        helper(root.left,str);
+        helper(root.right,str);
+    }
+}
