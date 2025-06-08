1d27377784c074f1b6a88294fde39bc402fc1396
anushkaa-dubey <anushka25dubey@gmail.com>
2025-06-08 22:11:26 +0530

    day40

---
+++ DAY40/program2.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/rotate-string/description/
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!= goal.length()) return false;
+        String combined = s + s; 
+        return combined.contains(goal);
+    }
+}
